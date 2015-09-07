138ff4ac704a041587cab137d0047873d76ce01c
Nanoo <gtempus@codegaloot.com>
2015-09-07 17:26:37 +0000

    add the beginning of grid.

--- grid.rb
+++ grid.rb
@@ -1,3 +1,15 @@
+require 'cell'
+
+class Grid
+  attr_reader :rows, :columns
+
+  def initialize(rows, columns)
+    @rows = rows
+    @columns = columns
+
+    @grid = prepare_grid
+    configure_cells
+  end
   def random_cell
     row = rand(@rows)
     column = rand(@grid[row].count)
