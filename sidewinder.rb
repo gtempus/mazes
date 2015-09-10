ae2c5a5ac7e589b49f413edd3e01c17f8afb65e9
Nanoo <gtempus@codegaloot.com>
2015-09-10 11:09:34 +0000

    Finish Sidewinder algorithm.

--- sidewinder.rb
+++ sidewinder.rb
@@ -1,13 +1,29 @@
-  class Sidewinder   
+class Sidewinder   
    
-    def self.no(grid)
-      grid.each_row do |row|
-        run = []
+  def self.on(grid)
+    grid.each_row do |row|
+      run = []
 
-        row.each do |cell|
-          run << cell
+      row.each do |cell|
+        run << cell
 
-          at_eastern_boundary  = (cell.east == nil)
-          at_northern_boundary = (cell.north == nil)       
-          
+        at_eastern_boundary  = (cell.east == nil)
+        at_northern_boundary = (cell.north == nil)       
+      
+        should_close_out =
+          at_eastern_boundary ||
+          (!at_northern_boundary && rand(2) == 0)       
    
+        if should_close_out
+          member = run.sample
+          member.link(member.north) if member.north
+          run.clear
+        else
+          cell.link(cell.east)
+        end
+      end
+    end   
+    
+    grid
+  end
+end
